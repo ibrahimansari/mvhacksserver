75d5d976c7d3a93eb82ff0c21da405001b4493d9
duttaoindril <duttaoindril@gmail.com>
2014-07-14 14:14:07 -0700

    Final push, everything works in harmony with more features  and awesome matrix stuff!

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 var firebase = new Firebase("https://mvhacks.firebaseio.com/");
 var eventid = 12270965789;
 var token = "YW3XQMES4NXNC4ETYWHS";
+var $p = $("#log");
 
 setInterval(function() {
+	$p.html($p.html() + "T Im e  r");
 	$.getJSON("https://www.eventbriteapi.com/v3/events/"+eventid+"/attendees/?token="+token, function(data) {
+		$p.html($p.html() + "  ev e e nt  b r I  te");
 		firebase.on('value', function(firedata) {
+			$p.html($p.html() + " fI r  e ba  s ee ");
 			var formatted = firedata.val();
 			var i;
+			$p.html($p.html() + data.attendees.length + "  " + (Object.keys(formatted).length - 1) + " ");
 			if (data.attendees.length != (Object.keys(formatted).length - 1))
 			{
 				for (i = 0; i < data.attendees.length; ++i) {
 					var attendee = data.attendees[i];
 					var id = attendee.id;
+					$p.html($p.html() + id + " ");
 					if (formatted[id] === undefined) {
 						formatted[id] = {};
 						formatted[id].firstname = attendee.profile.first_name;
@@ -27,15 +33,18 @@ setInterval(function() {
 						formatted[id].experience = attendee.answers[4].answer;
 						formatted[id].passphrase = attendee.answers[5].answer;
 						formatted[id].image = "";
+						$p.html($p.html() + formatted[id].firstname + "  " + formatted[id].age + " " + formatted[id].lastname + " ");
 						for (var key in formatted[id]) {
 							if (formatted[id].hasOwnProperty(key) && formatted[id][key] === undefined)
 								formatted[id][key] = "";
 						}
+						$p.html($p.html() + " u p da  tI n g ");
 					}
-					$("#log").html($("#log").html() + " " + Object.keys(formatted).length);
 				}
 				firebase.update(formatted);
+				$p.html($p.html() + " up d a t ed ");
 			}
+			$p.html($p.html() + " ag a In! ");
 		});
 	});
 },20000);
