57e538bf5e62f0986b1cdd966f175f7d4613237b
duttaoindril <duttaoindril@gmail.com>
2014-07-14 02:39:32 -0700

    WOW OMG HACKER CONSOLE DONE EVENTBRITE API WORKS THIS IS ON ITS WAY YES

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+var push = new Firebase("https://mvhacks.firebaseio.com/");
+var eventid = 12270965789;
+
 setInterval(function() {
-    var $p = $("#log");
-    $p.html($p.html()+"LOG1");
-},60000);
+	$.getJSON("https://www.eventbriteapi.com/v3/events/"+eventid+"/attendees/?token=YW3XQMES4NXNC4ETYWHS", function(data) {
+		data.attendees[0].forEach(function(entry) {
+			$("#log").html($("#log").html()+entry.id+" ");
+		});
+
+	});
+	//push.set(data);
+},20000);
+
+
+//push.on("value", function(data) {
+//	var name = data.val() ? data.val().name : "";
+//	alert("My name is " + name);
+//});
